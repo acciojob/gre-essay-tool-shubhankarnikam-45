70992da6ef30d0b509605a32549251c0c47e4be8
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-01-27 08:47:48 +0530

    add req. coding

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+
+let input=document.getElementById("evaluatedText");
+let output=document.getElementById("wordCount");
+
+input.addEventListener("keyup",evaluatedTextLength)
+
+function evaluatedTextLength()
+{
+    console.log("sh")
+    let text=input.value;
+    console.log(text)
+    // text.trim();
+    let arr=text.split(" ");
+    console.log(arr)
+
+    output.innerHTML=arr.length-1;
+}
